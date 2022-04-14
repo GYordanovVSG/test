86b412d7f96f937aa8033cb575d8b8c6cbb82679
VSG\Georgi <gyordanov@jhacorp.com>
2022-04-14 14:56:57 +0300

    added map

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(String name, String dayOfWeek) {
-  bat "echo Hello World ${name}. It is ${dayOfWeek}."
+def call(Map config = [:]) {
+  bat "echo Hello ${config.name}. It is ${config.dayOfWeek}."
 }
